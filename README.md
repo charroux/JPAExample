aedd62e56b7c094e109fcb005e72dbab19ea1b91
BenOrcha <benoit.charroux@gmail.com>
2019-02-06 12:18:59 +0100

    h2 console

--- README.md
+++ README.md
@@ -10,3 +10,8 @@ Import the project inside Eclipse: File -> Import -> General -> Existing project
 
 Launch the main program: https://github.com/charroux/JPAExample/blob/master/src/main/java/com/efrei/JPAExample/JpaExampleApplication.java
 
+
+Open the H2 web console: http://localhost:8080/h2-console
+
+In the login page make sure that you use jdbc:h2:mem:testdb as JDBC URL.
+
--- src/main/java/com/efrei/JPAExample/JpaExampleApplication.java
+++ src/main/java/com/efrei/JPAExample/JpaExampleApplication.java
@@ -3,18 +3,17 @@ package com.efrei.JPAExample;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.print.attribute.standard.PrinterLocation;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpaExampleApplication {
-
+	
 	private static final Logger log = LoggerFactory.getLogger(JpaExampleApplication.class);
 
 	public static void main(String[] args) {
